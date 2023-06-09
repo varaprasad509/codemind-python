6b3eee0cc688d139065ae2417f2c2330251d162b
varaprasad509 <134924836+varaprasad509@users.noreply.github.com>
2023-06-09 10:45:41 +0530

    n=input().lower().split() n="".join(n) a=sorted(n) c=[] for i in a:     if i not in c:         c.append(i) for i in c:     print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,9 +1,9 @@
 n=input().lower().split()
 n="".join(n)
-s=sorted(n)
+a=sorted(n)
 c=[]
-for i in s:
-    if s.count(i)==1:
+for i in a:
+    if i not in c:
         c.append(i)
 for i in c:
     print(i,end="")
