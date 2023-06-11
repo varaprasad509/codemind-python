8e44378668f7081f3665c44acd350fd4bb6934b5
varaprasad509 <134924836+varaprasad509@users.noreply.github.com>
2023-06-11 16:03:16 +0530

    s=input().lower().split() l=s[1:len(s)] d=0 m="" for i in s[0]:     for j in l:         if i in j:             d+=1             z=j.index(i)             q=l.index(j)             l[q]=j[0:z]+j[z+1:]     if len(l)==d:         m=m+i     d=0 if len(m)!=0:     print(len(m)) else:     print("0")

---
+++ Common_characters_-_2.py
@@ -0,0 +1,18 @@
+s=input().lower().split()
+l=s[1:len(s)]
+d=0
+m=""
+for i in s[0]:
+    for j in l:
+        if i in j:
+            d+=1
+            z=j.index(i)
+            q=l.index(j)
+            l[q]=j[0:z]+j[z+1:]
+    if len(l)==d:
+        m=m+i
+    d=0
+if len(m)!=0:
+    print(len(m))
+else:
+    print("0")
